cf90f2cc6a08ddfcd129d5c4e9e2c4f0fff2ed48
Kei-One01 <kouki4122@icloud.com>
2022-12-13 11:16:11 +0900

    ヘッダー変更前

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,16 +1,20 @@
-import { Link,Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function Header() {
     return (
         <div>
-            <div className="header">
-                <Link to={'/'} className="clear_Link_css" >KeyCaps</Link>
-                <div>Category</div>
-                <div className="space" />
-                <div><Link to={'/Login'} className="clear_Link_css" >Login</Link></div>
-                <div><Link to={'/Cart'} className="clear_Link_css" >Cart</Link></div>
-                <div>Search</div>
-            </div>
+            <body>
+                <header className="header">
+                    <h5>
+                        <Link to={'/'} className="clear_Link_css" >KeyCaps</Link>
+                    </h5>
+                    <div>Category</div>
+                    <div className="space" />
+                    <div><Link to={'/Login'} className="clear_Link_css" >Login</Link></div>
+                    <div><Link to={'/Cart'} className="clear_Link_css" >Cart</Link></div>
+                    <div>Search</div>
+                </header>
+            </body>
             <Outlet />
             {/* ここにルーター内のリンクに対応した各ページを表示させる */}
         </div>
